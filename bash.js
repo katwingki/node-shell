87d5ee9442249eb06257372fb71395ebb9358210
Betsy Groton <grotoned@gmail.com>
2020-08-07 12:35:40 -0400

    added pwd command

---
+++ bash.js
@@ -0,0 +1,14 @@
+/** @format */
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+	const cmd = data.toString().trim();
+	if (cmd === 'pwd') {
+		const pwd = process.cwd();
+		process.stdout.write(pwd);
+	} else {
+		process.stdout.write('You typed: ' + cmd);
+	}
+	process.stdout.write('\nprompt > ');
+});
